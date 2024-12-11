c7ae42c6d7550ad00660e8da6c85dda5d7e03c15
ogar_m <ogar.m.v@maio.ru>
2024-12-11 09:41:38 +0300

    Basket

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product("Ручной эспандер", 2500, 0.3);
-        Order firstOrder = new Order();
-        firstOrder.addProductWithDiscount(product);
-        Order secondOrder = new Order();
-        secondOrder.addProduct(product);
+//        Product product = new Product("Ручной эспандер", 2500, 0.3);
+//        Order firstOrder = new Order();
+//        firstOrder.addProductWithDiscount(product);
+//        Order secondOrder = new Order();
+//        secondOrder.addProduct(product);
+//
+//        Product secondProduct = new Product("Синий пояс по БЖЖ", 3500, 0.5);
+//
+//        Product thirdProduct = new Product("Ги для БЖЖ", 12500, 5);
+//
+//        Basket firstBasket = new Basket(product);
+//
+//        Basket secondBasket = new Basket(secondProduct);
+//        secondBasket.add(thirdProduct);
+//
+//        System.out.println("Первая корзина: " + firstBasket + "\n");
+//        System.out.println("Вторая корзина: " + secondBasket + "\n");
+//        System.out.println("Кол-во корзин: " + Basket.getCountBasket());
 
-        Product secondProduct = new Product("Синий пояс по БЖЖ", 3500, 0.5);
+        DeliveryOrder deliveryOrder = new DeliveryOrder(DeliveryType.PEDESTRIAN, IsFragile.NO, IsCold.YES);
+        System.out.println(deliveryOrder.toString());
 
-        Product thirdProduct = new Product("Ги для БЖЖ", 12500, 5);
-
-        Basket firstBasket = new Basket(product);
-
-        Basket secondBasket = new Basket(secondProduct);
-        secondBasket.add(thirdProduct);
-
-        System.out.println("Первая корзина: " + firstBasket + "\n");
-        System.out.println("Вторая корзина: " + secondBasket + "\n");
-        System.out.println("Кол-во корзин: " + Basket.getCountBasket());
     }
 }
